91e4b57015f3e69961baa95f0df9d03fdabc78fa
heappynd <heappynd@163.com>
2025-10-28 21:58:51 +0800

    String

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
-// use csv_to_json::{functions::read_csv, models::structs::HousePrice};
-
-use std::{iter, slice::Iter};
-
 fn main() {
-    let _v1: Vec<i32> = Vec::new();
-    let _v2 = vec![1, 2, 3];
+    let str = "Hello, world!";
+    let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = String::from("initial contents");
 
-    // let mut v3 = Vec::new();
-    // v3.push(5);
-    // v3.push(6);
-    // println!("{:?}", v3);
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    s1.push('c');
+    println!("{}", s1);
+    println!("{}", s2);
 
-    let i = _v2[1];
-    println!("{}", i);
-    let i1 = _v2.get(3);
-    match i1 {
-        Some(i) => println!("{}", i),
-        None => println!("No value"),
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // fn add(self, s: &str) -> String
+    println!("{}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    // let s = s1 + "-" + &s2 + "-" + &s3;
+    // println!("{}", s);
+    let s = format!("{s1}--{s2}--{s3}");
+    println!("{}", s);
+
+    let s1 = String::from("Здравствуйте"); // 24 bytes
+    // let h = s1[0]; // error: cannot index into a `String`
+    let s = &s1[0..4]; // slice of first character
+    println!("{}", s);
+    for c in "我".chars() {
+        println!("{}", c);
     }
-
-    let v: Vec<i32> = vec![1,2];
-    let mut iter: Iter<'_, i32> = v.iter();
-    let n1 = iter.next().unwrap();
-    let n2 = iter.next().unwrap();
-    let n3 = iter.next();
-    println!("{}, {}, {:?}", n1, n2, n3);
+    for b in "我".bytes() {
+        println!("{}", b);
+    }
 }
